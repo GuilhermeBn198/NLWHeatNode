29642c2079ff9ca425e58d147746fea007144a81
Guilherme Bernardo <bguilherme51@gmail.com>
2021-11-04 10:50:31 -0400

    get access_token from github, get user info from github in authentication service.

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 
 const router = Router();
 
-router.post("/authenticate", (req, res) => {})
+router.post("/authenticate",  new AuthenticateUserController().handle);
+
+export {router};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,18 +1,30 @@
 import axios from "axios";
 /**
  * receber code(string) ✔️
- * recuperar o access_token no github
- *  verificar se o user existe no db
+ * recuperar o access_token no github ✔️
+ * recuperar infos do user no github ✔️
+ *  verificar se o user existe no db 
  * ---- se existir = gera 1 token
  * ---- se não existir = cria o user no db e gera um token
  * retorna o token com as infos do user
  */
 
+interface IAccessTokenResponse {
+	access_token: string
+}
+
+interface IUserResponse{
+	avatar_url: string,
+	login: string,
+	id: number,
+	name: string
+}
+
 class AuthenticateUserService{
 	async execute(code: string ) {
 		const url = "https://github.com/login/oauth/access_token";
 
-		const response = await axios.post(url, null, { 
+		const { data:  accessTokenResponse} = await axios.post<IAccessTokenResponse>(url, null, { 
 			params: {
 				client_id: process.env.GITHUB_CLIENT_ID,
 				client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -22,6 +34,13 @@ class AuthenticateUserService{
 				"Accept": "application/json"
 			}
 		})
+
+		const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+			headers: {
+				authorization: `Bearer ${accessTokenResponse.access_token}`,
+			},
+		});
+
 		return response.data;
 	}
 }
